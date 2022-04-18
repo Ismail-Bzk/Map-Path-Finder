c267e07b349b82cfc968e716790a24d51c2a5379
Ismail-Bzk <103443731+Ismail-Bzk@users.noreply.github.com>
2022-04-19 01:00:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ int main() {
 
 
     Graph g;
-    read_record(g,"f1.txt");//g.readGraphFromFile("f1.txt");
+    read_record(g,"graph.txt");/
 
     cout << "Print Graph Operation -" << endl;
     g.printGraph();
 
-    int source =0, dest =20;
+    int source =86771, dest =110636;
     printShortestDistance(g, source, dest); // for BfS algorithm
     //shortestPathD(g,  73964,110636); // Dijkstra algorithm
-     //  86771,110636
+
 
   return 0;
 }
